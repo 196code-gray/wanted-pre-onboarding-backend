9c802d977240542dfb62c51efd7c9c0db8fa1866
196code-gray <doco827@gmail.com>
2023-08-14 22:38:04 +0900

    [feat] error 처리 로직

--- src/main/java/com/onboarid/wanted/exception/ExceptionCode.java
+++ src/main/java/com/onboarid/wanted/exception/ExceptionCode.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 public enum ExceptionCode {
     BOARD_NOT_FOUND(404, "게시글이 존재하지 않습니다."),
     USER_NOT_FOUND(404, "회원이 존재하지 않습니다."),
-    UNAUTHORIZED(401, "권한이 없습니다.");
+    UNAUTHORIZED(401, "권한이 없습니다."),
+    UNSUPPORTED_MEDIA_TYPE(402, "허용되지 않는 데이터 타입입니다."),
+    METHOD_NOT_ALLOWED(402, "허용되지 않는 요청입니다.");
 
     @Getter
     public int statusCode;
