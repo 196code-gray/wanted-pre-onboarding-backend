31a99ca262526ef05315b47565bc3198b6f80cfc
196code-gray <doco827@gmail.com>
2023-08-15 17:52:32 +0900

    [feat] spring security 설정파일

---
+++ src/main/java/com/onboarid/wanted/security/SecurityConfig.java
@@ -0,0 +1,95 @@
+package com.onboarid.wanted.security;
+
+import com.onboarid.wanted.security.jwt.CustomAuthorityUtils;
+import com.onboarid.wanted.security.jwt.filter.JwtAuthenticationFilter;
+import com.onboarid.wanted.security.jwt.JwtTokenizer;
+import com.onboarid.wanted.security.jwt.filter.JwtVerificationFilter;
+import com.onboarid.wanted.security.jwt.handler.UserFailureHandler;
+import com.onboarid.wanted.security.jwt.handler.UserSuccessHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity(debug = false)
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtTokenizer jwtTokenizer;
+    private final CustomAuthorityUtils authorityUtils;
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
+        http
+                .formLogin().disable()
+                .csrf().disable()
+                .httpBasic().disable()
+                .cors(withDefaults())
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .apply(new CustomFilterConfigurer())
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET,"/boards/**").permitAll()
+                .antMatchers("/boards/**").hasRole("USER")
+                .anyRequest().permitAll();
+
+        return http.build();
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOriginPattern("http://localhost:8080");
+        configuration.addAllowedOriginPattern("http://ec2-13-124-55-108.ap-northeast-2.compute.amazonaws.com");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.addExposedHeader("Authorization");
+        configuration.addExposedHeader("Refresh");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+    public class CustomFilterConfigurer extends AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity builder) throws Exception {
+            AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
+
+            JwtAuthenticationFilter jwtAuthenticationFilter =
+                    new JwtAuthenticationFilter(authenticationManager, jwtTokenizer);
+            jwtAuthenticationFilter.setFilterProcessesUrl("/login");
+            jwtAuthenticationFilter.setAuthenticationSuccessHandler(new UserSuccessHandler());
+            jwtAuthenticationFilter.setAuthenticationFailureHandler(new UserFailureHandler());
+
+            JwtVerificationFilter jwtVerificationFilter = new JwtVerificationFilter(jwtTokenizer, authorityUtils);
+
+            builder
+                    .addFilter(jwtAuthenticationFilter)
+                    .addFilterAfter(jwtVerificationFilter, JwtAuthenticationFilter.class);
+        }
+
+    }
+}
